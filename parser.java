744a7fe027459826fb010a55cd2cb3c875e43192
Death <death@golbez.uchicago.edu>
2015-08-07 15:35:34 -0500

    finished v1.0 of csv reader

--- parser.java
+++ parser.java
@@ -1,5 +1,5 @@
 import java.util.*;
-import java.io.*
+import java.io.*;
 
 /* *************************************************
                       Data Classes
@@ -25,7 +25,7 @@ class item {
 /* group class to hold all items of a certain type, i.e. Laptops */
 class group {
     private String name;
-    private List items;
+    private LinkedList items;
     private int num_items;
 
     public group(String name) {
@@ -33,26 +33,67 @@ class group {
         this.items = new LinkedList();
         this.num_items = 0;
     }
+
     public String getName() { return name; }
     public LinkedList getItems() { return items; }
     public int getNum_Items() { return num_items; }
+
+    public void addItem(item i) {
+        this.items.add(i);
+        this.num_items += 1;
+    }
 }
 
 /* *************************************************
                      Initializers
    ************************************************* */
-public class fileItems {
+class fileItems {
     private LinkedList groups;
     private int num_groups;
 
     public fileItems() {
         this.groups = new LinkedList();
-        this.num_items = 0;
+        this.num_groups = 0;
     }
-    public static void getItems() {
-        /* returns csv file of names and bib numbers */
-        /* create an group class for each group */
-        /* create an item class for each item */
+    
+    public void addGroup(group g) {
+        this.groups.add(g);
+        this.num_groups += 1;
+    }
+    public LinkedList getGroups() { return groups; }
+    public int getNum_Groups() { return num_groups; }
+
+
+    public void fetchItems() {
+        /* read csv file of names and bib numbers */
+        BufferedReader reader;
+        String ln;
+        String title;
+        String bibno;
+        group currGroup = null;
+        try {
+            reader = new BufferedReader(new FileReader("biblist.csv"));
+            while ((ln = reader.readLine()) != null) {
+                if (ln.isEmpty()) {}
+                else if (ln.substring(0,2).equals("//")) {
+                    /* create a group class for each group */
+                    title = ln.substring(3,(ln.length()-3));
+                    currGroup = new group(title);
+                    this.addGroup(currGroup);
+                }
+                else {
+                    /* create an item class for each item */
+                    String[] parts = ln.split(",");
+                    currGroup.addItem(new item(parts[0],parts[1]));
+                }
+            }
+        }
+        catch (FileNotFoundException fne) {
+            System.err.println("File Not Found Exception: " + fne.getMessage());
+        }
+        catch (IOException ioe) {
+            System.err.println("IO Exception" + ioe.getMessage());
+        }
     }
 }
 /* *************************************************
@@ -64,4 +105,3 @@ public class fileItems {
 /* *************************************************
                     Make it Pretty
    ************************************************* */
-
